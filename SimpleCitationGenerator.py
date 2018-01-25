482585f92ef35569beb1ccb487f6b2387804a178
SyntaxError528 <21lhammond@mtbluersd.org>
2018-01-25 14:19:43 -0500

    APA Citation added

---
+++ SimpleCitationGenerator.py
@@ -0,0 +1,17 @@
+import sys
+exit = 0
+print("Supported Citations: APA")
+while exit != 1:
+    decidingCitation = input("Enter name of citation: ")
+    if decidingCitation == "APA":
+        Firstname = input("Enter First name of author: ")
+        Lastname = input("Enter last name of author: ")
+        Title = input("Enter the title of the website: ")
+        URL = input("Enter the URL here: ")
+        Month = input("Enter the month here(Number): ")
+        Day = input("Enter the day here: ")
+        Year = input("Enter the year here(last 2 digits): ")
+
+        print(Lastname + ", " + Firstname + ", " + Title + "." + "[Online] " + URL + ", " + Month + "/" + Day + "/" + Year + ".")
+    if decidingCitation == "exit":
+        sys.exit()
