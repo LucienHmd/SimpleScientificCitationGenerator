b2b60bf6dc9f04f35c9595384334eba097830156
SyntaxError528 <legoman@myfairpoint.net>
2018-10-31 14:57:15 -0400

    Added MLA format

--- SimpleCitationGenerator.py
+++ SimpleCitationGenerator.py
@@ -3,8 +3,9 @@
 import sys
 
 exit = 0
-print("Supported Citations: APA")
+
 while exit != 1:
+    print("Supported Citations: APA and MLA")
     decidingCitation = input("Enter name of citation: ")
 
     if decidingCitation == "APA":
@@ -17,8 +18,23 @@ while exit != 1:
         Year = input("Enter the year here(last 2 digits): ")
 
         print(Lastname + ", " + Firstname + ", " + Title + "." + "[Online] " + URL + ", Accessed: " + Month + "/" + Day + "/" + Year + ".")
+        enter = input('Hit enter to continue...')
+
     if decidingCitation == "MLA":
-        
+        Firstname = input("Enter First name of author: ")
+        Lastname = input("Enter last name of author: ")
+        Websitename = input("Enter the name of the website: ")
+        Title = input("Enter the title of the website: ")
+        URL = input("Enter the URL here: ")
+        Month = input("Enter the month here(First 3 letters): ")
+        Day = input("Enter the day here: ")
+        Year = input("Enter the year here: ")
+        Publisher = input("Enter the name of the publisher: ")
+
+        print(Lastname + ", " + Firstname + ". " + '"' + Title + '." ' + Websitename + '(you need to italicize this)' + ', ' + Publisher + ', ' + Day + " " + Month + ". " + Year + ", " + URL + ".")
+
+        enter = input('Hit enter to continue...')
+
     if decidingCitation == "exit":
         sys.exit()
     
---
+++ Todo.md
@@ -0,0 +1,4 @@
+TO DO
+
+MLA month number converted to actual month abbriviation
+Use pydb to save citations for later use + be able to access it later
